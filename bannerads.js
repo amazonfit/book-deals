fe222ebed69da72b2d8b02dbb0ec52bd6fb626c8
amazonfit <pankaj20.sharma@gmail.com>
2016-11-15 22:40:10 +0530

    Create bannerads.js

---
+++ bannerads.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+// exampleHTMLToPage
+var exampleHTMLToPage = ExpandableBanners.banner("exampleHTMLToPage", "html/expanded.html", 728, 500);
+exampleHTMLToPage.setCloseImage("images/closebutton.jpg", 'right', 'bottom');
+exampleHTMLToPage.animated = true;
+exampleHTMLToPage.setDirection('down', 'right'); 
+</script>
